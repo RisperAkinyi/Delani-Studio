112d01d58556c7fd5c4345cd706569ae58ea0afa
Risper Akinyi <email@example.com>
2019-06-10 11:34:21 +0300

    Add JQuery function to what we do section

--- js/script.js
+++ js/script.js
@@ -1,8 +1,37 @@
 $(document).ready(function(){
-    $('#design').click(function(){
-        $('#design').toggleid('design')
-    })
-    $('#design').click(function(){
-        $('#info').toggleid('info')
-    })
-})
+    $("#design").click(function () { 
+        $("#design").hide();
+        $("#info").show();
+    });
+    $("#info").click(function () { 
+        $("#info").hide();
+        $("#design").show();
+    });
+
+    $("#dvpt").click(function () { 
+        $("#dvpt").hide();
+        $("#info1").show();        
+    });
+    $("#info1").click(function () { 
+        $("#info1").hide();
+        $("#dvpt").show();        
+    });
+
+    $("#pm").click(function () { 
+        $("#pm").hide();
+        $("#info2").show();        
+    });
+    $("#info2").click(function () { 
+        $("#info2").hide();
+        $("#pm").show();        
+    });
+
+    $(".card").hover(function () {
+        $(this).children(".parag1").fadeToggle().css({
+            "opacity":".5"
+            "text-align":"center"
+            "
+        });
+      });
+
+});
