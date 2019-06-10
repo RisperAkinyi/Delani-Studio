76c77d1a81ef73280821958daee6015ee463f262
Risper Akinyi <email@example.com>
2019-06-10 17:48:11 +0300

    Add hover effect to portfolio using jquery

--- js/script.js
+++ js/script.js
@@ -26,9 +26,61 @@ $(document).ready(function(){
         $("#pm").show();        
     });
 
+    $(".pt1").mouseenter(function(){
+        $(".parag1").show();
+    })
+    $(".pt1").mouseleave(function () { 
+        $(".parag1").hide();        
+    });
+    $(".pt2").mouseenter(function(){
+        $(".parag2").show();
+    })
+    $(".pt2").mouseleave(function () { 
+        $(".parag2").hide();        
+    });
+    $(".pt3").mouseenter(function(){
+        $(".parag3").show();
+    })
+    $(".pt3").mouseleave(function () { 
+        $(".parag3").hide();        
+    });
+    $(".pt4").mouseenter(function(){
+        $(".parag4").show();
+    })
+    $(".pt4").mouseleave(function () { 
+        $(".parag4").hide();        
+    });
+    $(".pt5").mouseenter(function(){
+        $(".parag5").show();
+    })
+    $(".pt5").mouseleave(function () { 
+        $(".parag5").hide();        
+    });
+    $(".pt6").mouseenter(function(){
+        $(".parag6").show();
+    })
+    $(".pt6").mouseleave(function () { 
+        $(".parag6").hide();        
+    });
+    $(".pt7").mouseenter(function(){
+        $(".parag7").show();
+    })
+    $(".pt7").mouseleave(function () { 
+        $(".parag7").hide();        
+    });
+    $(".pt8").mouseenter(function(){
+        $(".parag8").show();
+    })
+    $(".pt8").mouseleave(function () { 
+        $(".parag8").hide();        
+    });
+
+
     $("#button").click(function(){
         alert("Your message has been received...");
     });
+
+    
 
     
 });
